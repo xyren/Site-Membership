71b98a647b7d9aa39e96fe9225b4818862a96b6d
xyren <jenner.alagao@gmail.com>
2015-09-17 07:55:31 +0800

    Admin account add - form validation added

--- site-member/add.php
+++ site-member/add.php
@@ -1,72 +1,98 @@
 ﻿<div class="wrap">	
-	<h1 id="add-new-user">Add New User</h1>
+	<h1 id="add-new-user">Add New 
 		<?
 		$_levelID = site_members::get_levelID_from_url();
 		$_level = site_members::memberLevel($_levelID);
 		if(!empty($_level)){
-			echo "Add ".$_level;
+			echo $_level;
 		}else{
 			die('Request Member Level not found.');
 		}?>
 	</h1>
+	
+	
 	<p>Create a brand new <?=$_level;?> and add them to this site.</p>
+	
+	<div id="ajax-response"></div>
+	
 	
 	<?
 	
-	$now = current_time('mysql');
+$_error = '';
+if(isset($_POST['submit'])){
+	
+	$_data = array();
+	foreach($_POST as $key => $val){
+		$_data[$key] = esc_attr($val);
+	}
+	
+	//check empty duplicates already process by function below
+	$_result = account::add_validate($_POST,$_levelID);
+	if(is_array($_result)){
+		$_error = $_result;
+		echo '<div id="message" class="error notice is-dismissible">';
+		echo '<p>';
+		print_r($_error);
+		echo '</p></div>';
+	}else{
+		echo '<div id="message" class="updated notice is-dismissible">';
+		echo '<p>Success! new account added.</p>';
+		echo '</div>';
+		unset($_data); //remove all post data.
+	}
+}
 
-	global $wpdb;
-	$_temp ='the_x_con';
-		$userdata = array(
-			'user_pass' => esc_attr($_temp),
-			'user_login' => esc_attr($_temp),
-			'first_name' => esc_attr($_temp .'first'),
-			'last_name' => esc_attr($_temp.' last'),
-			'user_email' => esc_attr($_temp .'@email.com'),
-			'role' => 'modulator'
-		);
-		$new_user = wp_insert_user( $userdata );
-		
-		$wpdb->insert(MEMBERS_TABLE , array('userID'=>$new_user , 'member_accepted'=>$now , 'level_id' => 4));
-		$insert_id= $wpdb->insert_id;
+
+?>
+<style>
+.form-required td .description{color:#dc3232 !important};
+</style>
+
+<form name="createuser" id="createuser" method="post" class="validate" novalidate="novalidate">
+	<? wp_nonce_field(MEMBERS_SECRET.'_account_signup'); ?>
+	<input type="hidden" name="action" id="action" value="sitemember_account_add_admin">
 	
-		?>
+	<table class="form-table">
+		<tr class="form-field form-required <?=isset($_error['user']) ? 'form-invalid' : '';?>">
+			<th scope="row"><label for="user">Username <span class="description">(required)</span></label></th>
+			<td><span class="description"><?=isset($_error['user']) ? $_error['user'].'<br/>' : '';?></span>
+			<input type="text" name="user" id="user" class="required" value="<?=$_data['user'];?>" required autocapitalize="none" autocorrect="off" >
+			</td>
+		</tr>
+		<tr class="form-field <?=!empty(isset($_error['pass'])) ? 'form-invalid' : '';?>">
+			<th scope="row"><label for="pass">Password </label></th>
+			<td><span class="description">Will send to your email-address. <?=isset($_error['pass']) ? $_error['pass'].'<br/>' : '';?></span>
+				<input type="hidden" name="pass" id="pass" class="" value="admin" aria-required="true" autocapitalize="none" autocorrect="off" >
+			</td>
+		</tr>
+		<tr class="form-field form-required <?=isset($_error['email']) ? 'form-invalid' : '';?>">
+			<th scope="row"><label for="email">E-mail <span class="description">(required)</span></label></th>
+			<td><span class="description"><?=isset($_error['email']) ? $_error['email'].'<br/>' : '';?></span>
+			<input type="text" name="email" id="email" class="" value="<?=$_data['email'];?>" aria-required="true" autocapitalize="none" autocorrect="off" >
+			</td>
+		</tr>
+		<tr class="form-field form-required <?=isset($_error['fname']) ? 'form-invalid' : '';?>">
+			<th scope="row"><label for="fname">First Name <span class="description">(required)</span></label></th>
+			<td><span class="description"><?=isset($_error['fname']) ? $_error['fname'].'<br/>' : '';?></span>
+			<input type="text" name="fname" id="fname" class="" value="<?=$_data['fname'];?>" aria-required="true" autocapitalize="none" autocorrect="off" >
+			</td>
+		</tr>
+		<tr class="form-field form-required <?=isset($_error['lname']) ? 'form-invalid' : '';?>">
+			<th scope="row"><label for="lname">Last Name <span class="description">(required)</span></label></th>
+			<td><span class="description"><?=isset($_error['lname']) ? $_error['lname'].'<br/>' : '';?></span>
+			<input type="text" name="lname" id="lname" class="" value="<?=$_data['lname'];?>" aria-required="true" autocapitalize="none" autocorrect="off" >
+			</td>
+		</tr>
+	</table>
 	
 	
-	<form>
- <input type="password" name="password" />
- <input type="password" name="password_retyped" />
- <span id="password-strength"></span>
- <input type="submit" disabled="disabled" value="Submit" />
+	<p class="submit">
+		<input type="submit" name="submit" id="submit" value="Add New <?=$_level;?>" class="button button-primary">
+	</p>
+	
 </form>
 
 
 
 
-
-<?
-	
-				
-				global $form_event_action, $form_action , $_subjID;
-				$form_event_action = 'question_add';
-				$_subjID = $_subjctID;
-			/* 	$form_action="add";
-				$fighterClass = new mma();
-				$fighterClass->setFigther();
-				$_fighter = $fighterClass->info();
-				$fighter = (object) $_fighter; */
-				include('form.php');
-				
-			
-	
-			echo 'Error: 
-				</h2><div class="error below-h2" id="message">
-				<p><b>Invalid request:</b> The subject your requesting to view doesn\'t exist in database.
-				</p>
-			</div>';
-		
-		
-		
-    ?>
-		
 	</div>
---
+++ template/account/signup_admin.php
@@ -0,0 +1,14 @@
+<?
+
+
+add_action('wp_print_scripts','member_scripts::member_form_add');
+
+//wp_enqueue_style('jquery'); 
+
+get_header();
+<script>
+
+
+</script>
+
+<? get_footer();?>
